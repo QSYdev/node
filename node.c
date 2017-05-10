6f80c26a06362c97811ad5d0f72c0a76a663530e
Ramiro <ramiroromero94@gmail.com>
2017-05-10 18:47:34 -0300

    Usar baudrate 115200

--- node.c
+++ node.c
@@ -7,24 +7,25 @@
 
 void node_notify(uint16_t event)
 {
-        switch(event) {
-        case GOT_IP:
-                discovery_start();
-                break;
-        case GOT_TERMINAL:
-                discovery_stop();
-                break;
-        case TERMINAL_LOST:
-                discovery_start();
-                break;
-        default:
-                break;
-        }
+	switch(event) {
+	case GOT_IP:
+		discovery_start();
+		break;
+	case GOT_TERMINAL:
+		discovery_stop();
+		break;
+	case TERMINAL_LOST:
+		discovery_start();
+		break;
+	default:
+		break;
+	}
 }
 
 void ICACHE_FLASH_ATTR user_init()
 {
+	uart_div_modify(0, UART_CLK_FREQ / 115200);
 	led_init();
 	wifi_init();
-        command_init();
+	command_init();
 }
