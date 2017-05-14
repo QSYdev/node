a8a022bace25d454e6ce2c306abeaf64028a7d50
Agus <agusss.garcia19@gmail.com>
2017-05-14 00:14:21 -0300

    Network fixed

--- network.h
+++ network.h
@@ -6,9 +6,9 @@ uint16_t inline ntohs(uint16_t s)
         return  (((0xFF) & s) << 8) | (((0xFF00) & s) >> 8);
 }
 
-uint16_t inline ntohl(uint32_t l)
+uint32_t inline ntohl(uint32_t l)
 {
-        return ntohs((l & 0xFFFF0000)>>16) | (ntohs(l & 0xFFFF) << 16)
+        return ntohs((l & 0xFFFF0000) >> 16) | (ntohs(l & 0xFFFF) << 16)
 }
 
 uint16_t inline htons(uint16_t s)
@@ -16,7 +16,7 @@ uint16_t inline htons(uint16_t s)
         return ntohs(s);
 }
 
-uint16_t inline htosl(uint32_t l)
+uint32_t inline htonl(uint32_t l)
 {
         return ntohl(l);
 }
