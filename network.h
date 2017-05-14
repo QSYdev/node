41da73bab52e2bd8988170187faf973f120dd2a8
Ramiro <ramiroromero94@gmail.com>
2017-05-13 23:41:02 -0300

    Conversion de bytes

---
+++ network.h
@@ -0,0 +1,25 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+
+uint16_t inline ntohs(uint16_t s)
+{
+        return  (((0xFF) & s) << 8) | (((0xFF00) & s) >> 8);
+}
+
+uint16_t inline ntohl(uint32_t l)
+{
+        return ntohs((l & 0xFFFF0000)>>16) | (ntohs(l & 0xFFFF) << 16)
+}
+
+uint16_t inline htons(uint16_t s)
+{
+        return ntohs(s);
+}
+
+uint16_t inline htosl(uint32_t l)
+{
+        return ntohl(l);
+}
+
+
+#endif
