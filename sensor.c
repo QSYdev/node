c3131c35238d8a51493f98698326a9439f29c2c4
Ramiro <ramiroromero94@gmail.com>
2017-06-03 01:12:10 -0300

    Touche. No probado

--- sensor.c
+++ sensor.c
@@ -4,8 +4,18 @@
 #include "gpio.h"
 #include "ets_sys.h"
 #include "sensor.h"
+#include "message.h"
+#include  "tcp_connection.h"
 
 static void sensor_cb();
+
+static uint32_t rtc_value;
+
+/* TODO: id? capaz deberiamos centralizar el armado de paquetes en un sólo módulo */
+static struct qsy_message touche_message =
+{
+	.signature = {'Q','S','Y'}
+};
 
 void ICACHE_FLASH_ATTR sensor_init(void) {
 }
@@ -17,6 +27,8 @@ void ICACHE_FLASH_ATTR sensor_start(void) {
 	ETS_GPIO_INTR_ATTACH(sensor_cb, NULL);
 
 	ETS_GPIO_INTR_ENABLE();
+
+	rtc_value = system_get_rtc_time();
 }
 
 void ICACHE_FLASH_ATTR sensor_disarm(void) {
@@ -28,7 +40,15 @@ void ICACHE_FLASH_ATTR sensor_disarm(void) {
 }
 
 static void sensor_cb(void) {
-	//TODO notificar a alguien cuando fue pulsado.
+	/* clocks*/
+	uint32_t delay = rtc_value - system_get_rtc_time();
+	/* microsegundos */
+	delay *= system_rtc_clock_cali_proc() >> 12;
+	/* milisegundos */
+	delay /= 1000; 
+
+	touche_message.delay = delay;
+	tcp_connection_send_message(&touche_message, sizeof(touche_message));
 
 	uint32_t gpio_status = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
 	GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, gpio_status);
